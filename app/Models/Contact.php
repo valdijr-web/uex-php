a57a8484bc05527d2eb1ed3196c77ea8c7c9670e
valdijunior <valdi.ads@gmail.com>
2025-02-26 15:22:05 -0300

    especifiquei o retorno do metodo de relacionamento.

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Contact extends Model
@@ -32,7 +33,7 @@ class Contact extends Model
         'longitude' => 'float'
     ];
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
